2836a609e3d8f9f3412211bd6c8f876533e42b63
Jesus <jesferrod1@alum.us.es>
2022-05-13 13:02:00 +0200

    EventResource

--- subproyectoIntegracion-l8-2/src/main/java/aiss/api/resources/EventResource.java
+++ subproyectoIntegracion-l8-2/src/main/java/aiss/api/resources/EventResource.java
@@ -1,5 +1,13 @@
 package aiss.api.resources;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -8,37 +16,34 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import org.jboss.resteasy.spi.BadRequestException;
 import org.jboss.resteasy.spi.NotFoundException;
 
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-
-import aiss.api.resources.comparators.ComparatorAlbumSong;
-import aiss.api.resources.comparators.ComparatorAlbumSongReversed;
-import aiss.api.resources.comparators.ComparatorArtistSong;
-import aiss.api.resources.comparators.ComparatorArtistSongReversed;
-import aiss.api.resources.comparators.ComparatorYearSong;
-import aiss.api.resources.comparators.ComparatorYearSongReversed;
-import aiss.model.Song;
-import aiss.model.repository.MapCityRepository;
+import aiss.api.resources.comparators.ComparatorCategoryEvent;
+import aiss.api.resources.comparators.ComparatorCategoryEventReversed;
+import aiss.api.resources.comparators.ComparatorDateEvent;
+import aiss.api.resources.comparators.ComparatorDateEventReversed;
+import aiss.api.resources.comparators.ComparatorDescriptionEvent;
+import aiss.api.resources.comparators.ComparatorDescriptionEventReversed;
+import aiss.api.resources.comparators.ComparatorLocationEvent;
+import aiss.api.resources.comparators.ComparatorLocationEventReversed;
+import aiss.api.resources.comparators.ComparatorOrganizerEvent;
+import aiss.api.resources.comparators.ComparatorOrganizerEventReversed;
+import aiss.api.resources.comparators.ComparatorPriceEvent;
+import aiss.api.resources.comparators.ComparatorPriceEventReversed;
+import aiss.model.Event;
 import aiss.model.repository.CityRepository;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import aiss.model.repository.MapCityRepository;
 
 
 
-@Path("/songs")
+@Path("/events")
 public class EventResource {
 
 	public static EventResource _instance=null;
@@ -57,55 +62,66 @@ public class EventResource {
 	
 	@GET
 	@Produces("application/json")
-	public Collection<Song> getAll(@QueryParam("q") String q, @QueryParam("order") String order,
+	public Collection<Event> getAll(@QueryParam("q") String q, @QueryParam("order") String order,
 			@QueryParam("limit") Integer limit, @QueryParam("offset") Integer offset)
 	{
-		List<Song> result = new ArrayList<Song>();
-		List<Song> songs = repository.getAllSongs().stream()
+		List<Event> result = new ArrayList<Event>();
+		List<Event> events = repository.getAllEvents().stream()
 				.collect(Collectors.toList());
 		int start = offset == null ? 0: offset - 1;
-		int end = limit == null ? songs.size(): start + limit;
+		int end = limit == null ? events.size(): start + limit;
 		
 		// Bloque de código de filtrado y de paginación
 		for(int i = start; i < end; i++) {
-			if(songs.get(i).getTitle().contains(q) || songs.get(i).getAlbum().contains(q)
-					|| songs.get(i).getArtist().contains(q)) {
+			if(events.get(i).getName().contains(q) || events.get(i).getCategory().contains(q)
+					|| events.get(i).getLocation().contains(q)) {
 				
-				result.add(songs.get(i));
+				result.add(events.get(i));
 			}
 		}
 		
-		/*for(Song song: repository.getAllSongs()) {
-			if(q == null || song.getTitle().contains(q) || song.getAlbum().contains(q)
-					|| song.getArtist().contains(q)) {
-				
-				result.add(song);
-			}
-		}*/
-		
 		// Bloque de código de ordenación
 		if(order != null) {
-			if(order.equals("album")) {
-				Collections.sort(result, new ComparatorAlbumSong());
+			if(order.equals("category")) {
+				Collections.sort(result, new ComparatorCategoryEvent());
 			}
-			else if(order.equals("-album")) {
-				Collections.sort(result, new ComparatorAlbumSongReversed());
+			else if(order.equals("-category")) {
+				Collections.sort(result, new ComparatorCategoryEventReversed());
 			}
-			else if(order.equals("artist")) {
-				Collections.sort(result, new ComparatorArtistSong());
+			else if(order.equals("date")) {
+				Collections.sort(result, new ComparatorDateEvent());
 			}
-			else if(order.equals("-artist")) {
-				Collections.sort(result, new ComparatorArtistSongReversed());
+			else if(order.equals("-date")) {
+				Collections.sort(result, new ComparatorDateEventReversed());
 			}
-			else if(order.equals("year")) {
-				Collections.sort(result, new ComparatorYearSong());
+			else if(order.equals("description")) {
+				Collections.sort(result, new ComparatorDescriptionEvent());
 			}
-			else if(order.equals("-year")) {
-				Collections.sort(result, new ComparatorYearSongReversed());
+			else if(order.equals("-description")) {
+				Collections.sort(result, new ComparatorDescriptionEventReversed());
+			}
+			else if(order.equals("location")) {
+				Collections.sort(result, new ComparatorLocationEvent());
+			}
+			else if(order.equals("-location")) {
+				Collections.sort(result, new ComparatorLocationEventReversed());
+			}
+			else if(order.equals("organizer")) {
+				Collections.sort(result, new ComparatorOrganizerEvent());
+			}
+			else if(order.equals("-organizer")) {
+				Collections.sort(result, new ComparatorOrganizerEventReversed());
+			}
+			else if(order.equals("price")) {
+				Collections.sort(result, new ComparatorPriceEvent());
+			}
+			else if(order.equals("-price")) {
+				Collections.sort(result, new ComparatorPriceEventReversed());
 			}
 			else {
-				throw new BadRequestException("The order parameter must be 'album', '-album', "
-						+ "'artist', '-artist', 'year', '-year'.");
+				throw new BadRequestException("The order parameter must be 'category', '-category', "
+						+ "'date', '-date', 'description', '-description', 'location', '-location'"
+						+ ", 'organizer', '-organizer', 'price', '-price'.");
 			}
 		}
 		
@@ -126,78 +142,91 @@ public class EventResource {
 	@GET
 	@Path("/{id}")
 	@Produces("application/json")
-	public Song get(@PathParam("id") String songId)
+	public Event get(@PathParam("id") String eventId)
 	{
-		Song song = repository.getSong(songId);
+		Event event = repository.getEvent(eventId);
 		
-		if(song == null) {
-			throw new NotFoundException("The song with id=" + songId + " was not found");
+		if(event == null) {
+			throw new NotFoundException("The event with id=" + eventId + " was not found");
 		}
 		
-		return song;
+		return event;
 	}
 	
 	@POST
 	@Consumes("application/json")
 	@Produces("application/json")
-	public Response addSong(@Context UriInfo uriInfo, Song song) {
-		if(song.getTitle() == null || "".equals(song.getTitle())) {
-			throw new BadRequestException("The name of the song must not be null");
+	public Response addEvent(@Context UriInfo uriInfo, Event event) {
+		if(event.getName() == null || "".equals(event.getName())) {
+			throw new BadRequestException("The name of the event must not be null");
 		}
 		
-		repository.addSong(song);
+		repository.addEvent(event);
 		
 		// Builds the response. Returns the song that has just been added.
 		UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
-		URI uri = ub.build(song.getId());
+		URI uri = ub.build(event.getId());
 		ResponseBuilder resp = Response.created(uri);
-		resp.entity(song);
+		resp.entity(event);
 		return resp.build();
 	}
 	
 	
 	@PUT
 	@Consumes("application/json")
-	public Response updateSong(Song song) {
-		Song oldSong = repository.getSong(song.getId());
+	public Response updateEvent(Event event) {
+		Event oldEvent = repository.getEvent(event.getId());
 		
-		if(oldSong == null) {
-			throw new NotFoundException("The song with id=" + song.getId() + " was not found");
+		if(oldEvent == null) {
+			throw new NotFoundException("The event with id=" + event.getId() + " was not found");
 		}
 		
-		// Update title
-		if(song.getTitle() != null) {
-			oldSong.setTitle(song.getTitle());
+		// Update name
+		if(event.getName() != null) {
+			oldEvent.setName(event.getName());
 		}
 		
-		// Update artist
-		if(song.getArtist() != null) {
-			oldSong.setArtist(song.getArtist());
+		// Update category
+		if(event.getCategory() != null) {
+			oldEvent.setCategory(event.getCategory());
 		}
 		
-		// Update album
-		if(song.getAlbum() != null) {
-			oldSong.setAlbum(song.getAlbum());
+		// Update description
+		if(event.getDescription() != null) {
+			oldEvent.setDescription(event.getDescription());
 		}
 		
-		// Update year
-		if(song.getYear() != null) {
-			oldSong.setYear(song.getYear());
+		// Update organizer
+		if(event.getOrganizer() != null) {
+			oldEvent.setOrganizer(event.getOrganizer());
 		}
+		// Update location
+		if(event.getLocation() != null) {
+			oldEvent.setLocation(event.getLocation());
+		}
+		// Update date
+		if(event.getDate() != null) {
+			oldEvent.setDate(event.getDate());
+		}
+		// Update price
+		if(event.getPrice() != null) {
+			oldEvent.setPrice(event.getPrice());
+		}
+				
 		
 		return Response.noContent().build();
 	}
 	
 	@DELETE
 	@Path("/{id}")
-	public Response removeSong(@PathParam("id") String songId) {
-		Song song = repository.getSong(songId);  // Obtenemos la canción a eliminar
+	public Response removeEvent(@PathParam("id") String eventId) {
+		Event event = repository.getEvent(eventId);  // Obtenemos el evento a eliminar
 		
-		if(song == null) {
-			throw new NotFoundException("The song with id=" + songId + " was not found");
+		if(event == null) {
+			throw new NotFoundException("The event with id=" + eventId + " was not found");
 		}
 		else {
-			repository.deleteSong(songId);
+			repository.deleteEvent(eventId);
 		}
 		
 		return Response.noContent().build();
